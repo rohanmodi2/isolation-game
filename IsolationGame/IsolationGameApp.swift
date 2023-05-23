f72a0aa6758a8cf38b2b7bb51d8fbc084906540a
Rohan Modi <rohanmodi@outlook.in>
2023-05-23 13:07:21 +0530

    Initial Commit

---
+++ IsolationGame/IsolationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsolationGameApp.swift
+//  IsolationGame
+//
+//  Created by Rohan Modi on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct IsolationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
